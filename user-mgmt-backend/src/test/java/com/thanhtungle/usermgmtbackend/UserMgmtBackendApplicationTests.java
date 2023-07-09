32d5c93e92e6cfb354bf834bcf7e1377627eda3d
Thanh Tung Le <thanhtungle73@gmail.com>
2023-07-10 01:25:28 +0700

    BE: init backend source

---
+++ user-mgmt-backend/src/test/java/com/thanhtungle/usermgmtbackend/UserMgmtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanhtungle.usermgmtbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMgmtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
