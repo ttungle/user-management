2c616c4362a08b5939dd84b79e291f38d04ad536
Thanh Tung Le <thanhtungle73@gmail.com>
2023-07-10 20:54:56 +0700

    BE: add getting all users api

--- user-mgmt-backend/src/main/java/com/thanhtungle/usermgmtbackend/controller/UserController.java
+++ user-mgmt-backend/src/main/java/com/thanhtungle/usermgmtbackend/controller/UserController.java
@@ -3,10 +3,9 @@ package com.thanhtungle.usermgmtbackend.controller;
 import com.thanhtungle.usermgmtbackend.model.User;
 import com.thanhtungle.usermgmtbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -15,8 +14,13 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/user")
-    User createUser(@RequestBody User newUser) {
+    @PostMapping("/users")
+    public User createUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
