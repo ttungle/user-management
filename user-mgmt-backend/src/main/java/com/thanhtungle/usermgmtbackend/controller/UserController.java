0b40cb6c4e550ca99b805fe82111d2e1f59f03c6
Thanh Tung Le <thanhtungle73@gmail.com>
2023-07-12 23:35:03 +0700

    BE: add edit an user api

--- user-mgmt-backend/src/main/java/com/thanhtungle/usermgmtbackend/controller/UserController.java
+++ user-mgmt-backend/src/main/java/com/thanhtungle/usermgmtbackend/controller/UserController.java
@@ -29,6 +29,17 @@ public class UserController {
     @GetMapping("/users/{id}")
     public User getUserById(@PathVariable Long id) {
 
-        return userRepository.findById(id).orElseThrow(() -> new NotFoundException("User with id " + id + " could not be found."));
+        return userRepository.findById(id).orElseThrow(() -> new NotFoundException("No user found with id " + id));
+    }
+
+    @PutMapping("/users/{id}")
+    public User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id).map(user -> {
+            user.setUsername(newUser.getUsername());
+            user.setName(newUser.getName());
+            user.setEmail(newUser.getEmail());
+
+            return userRepository.save(user);
+        }).orElseThrow(() -> new NotFoundException("No user found with id " + id));
     }
 }
